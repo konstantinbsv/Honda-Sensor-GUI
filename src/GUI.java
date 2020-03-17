9167379defafd7e711cd7eaea4ab69f2a241f648
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-17 08:45:10 -0700

    Add icon to stage

--- src/GUI.java
+++ src/GUI.java
@@ -2,11 +2,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
     private static final String stageTitle = "Sensor GUI";
     public static final String cssRes = "res/css/flat_dark_theme.css";
+    public static final String iconRes = "res/img/icon.png";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -14,6 +16,10 @@ public class GUI extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add(cssRes);
         primaryStage.setTitle(stageTitle);
+        primaryStage.getIcons().add(
+                new Image(
+                        getClass().getResourceAsStream(iconRes),
+                        100, 100, true, true)); // add icon
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
         primaryStage.show();
