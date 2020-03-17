aa8da7aac925305accc4917f67cac65e10522fb3
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-16 19:32:35 -0700

    Create Serial class and add dependencies

---
+++ src/Serial.java
@@ -0,0 +1,85 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Serial {
+
+    static SerialPort serialPort;
+    static InputStream streamIn;
+    static final String comPort = "COM8";
+    static final int baudRate = 115200;
+    static final int dataBits = 8;
+    static final int stopBits = 1;
+    static final int parity = 0;
+
+    static Logger logger = Logger.getLogger("Serial");
+
+    /**
+     * Retrieves next line from serial (stops when it reaches '\r' or '\n')
+     *
+     * @return String
+     */
+    static String getNextLine() {
+
+        char currentChar;
+        StringBuilder bufferString = new StringBuilder();
+
+        try {
+            do {
+                currentChar = (char) streamIn.read();
+                bufferString.append(currentChar);
+            } while (currentChar != '\r');
+        } catch (IOException io) {
+            System.out.println("IO exception: " + io.toString());
+        }
+
+        return bufferString.toString();
+    }
+
+
+    /**
+     * Send string over serial
+     * @param inputString String to be sent
+     */
+    static void sendStringUART(String inputString) {
+        logger.log(Level.INFO, inputString);        // send inputString to debugger
+        byte[] buffer = inputString.getBytes();
+
+        // send bytes in new thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                serialPort.writeBytes(buffer, buffer.length);
+            }
+        }).start();
+
+    }
+
+    /**
+     * Initializes serial communication
+     *
+     * @return true if initialization successful
+     */
+    static boolean initializeSerial() {
+
+        serialPort = SerialPort.getCommPort(comPort);
+        serialPort.setComPortParameters(baudRate, dataBits, stopBits, parity);
+        serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0 , 0);
+
+        /* Initialize COM port */
+        if (serialPort.openPort()) {
+            System.out.println("Port " + comPort + " opened successfully!");
+            // stmIn = new BufferedReader(new InputStreamReader(stmPort.getInputStream()));
+            streamIn = serialPort.getInputStream();
+            return true;
+        } else {
+            System.out.println("Port " + comPort + " unreachable");
+            // TODO: request new port from user
+            return false;
+        }
+    }
+
+}
