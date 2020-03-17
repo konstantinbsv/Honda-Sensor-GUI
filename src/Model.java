d2d0a95386b9f671e65973fb953b9ceab3ed810a
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-17 08:27:47 -0700

    Create setters for value variables

--- src/Model.java
+++ src/Model.java
@@ -7,6 +7,12 @@ public class Model {
 
     int dataPoint = 0;
 
+    // data points for current values
+    // private double[] pressuresLateral = new double[6];
+    // private double[] centerData = new double[4];    // pressure, proximity, shearX, shearY
+    private double pressure1, pressure2, pressure3, pressure4, pressure5, pressure6;
+    private double pressureCenter, proximity, shearX, shearY;
+
     // lateral graphs
     private XYChart.Series<Number, Number> pressure1Series = new XYChart.Series<>();
     private XYChart.Series<Number, Number> pressure2Series = new XYChart.Series<>();
@@ -35,79 +41,83 @@ public class Model {
         return pressure1Series;
     }
 
-    public void setPressure1Series(XYChart.Series<Number, Number> pressure1Series) {
-        this.pressure1Series = pressure1Series;
-    }
-
     public XYChart.Series<Number, Number> getPressure2Series() {
         return pressure2Series;
-    }
-
-    public void setPressure2Series(XYChart.Series<Number, Number> pressure2Series) {
-        this.pressure2Series = pressure2Series;
     }
 
     public XYChart.Series<Number, Number> getPressure3Series() {
         return pressure3Series;
     }
 
-    public void setPressure3Series(XYChart.Series<Number, Number> pressure3Series) {
-        this.pressure3Series = pressure3Series;
-    }
-
     public XYChart.Series<Number, Number> getPressure4Series() {
         return pressure4Series;
-    }
-
-    public void setPressure4Series(XYChart.Series<Number, Number> pressure4Series) {
-        this.pressure4Series = pressure4Series;
     }
 
     public XYChart.Series<Number, Number> getPressure5Series() {
         return pressure5Series;
     }
 
-    public void setPressure5Series(XYChart.Series<Number, Number> pressure5Series) {
-        this.pressure5Series = pressure5Series;
-    }
-
     public XYChart.Series<Number, Number> getPressure6Series() {
         return pressure6Series;
-    }
-
-    public void setPressure6Series(XYChart.Series<Number, Number> pressure6Series) {
-        this.pressure6Series = pressure6Series;
     }
 
     public XYChart.Series<Number, Number> getPressureCenterSeries() {
         return pressureCenterSeries;
     }
 
-    public void setPressureCenterSeries(XYChart.Series<Number, Number> pressureCenterSeries) {
-        this.pressureCenterSeries = pressureCenterSeries;
-    }
-
     public XYChart.Series<Number, Number> getProximitySeries() {
         return proximitySeries;
-    }
-
-    public void setProximitySeries(XYChart.Series<Number, Number> proximitySeries) {
-        this.proximitySeries = proximitySeries;
     }
 
     public XYChart.Series<Number, Number> getShearXSeries() {
         return shearXSeries;
     }
 
-    public void setShearXSeries(XYChart.Series<Number, Number> shearXSeries) {
-        this.shearXSeries = shearXSeries;
-    }
-
     public XYChart.Series<Number, Number> getShearYSeries() {
         return shearYSeries;
     }
 
-    public void setShearYSeries(XYChart.Series<Number, Number> shearYSeries) {
-        this.shearYSeries = shearYSeries;
+    public void setPressure1(double pressure1) {
+        this.pressure1 = pressure1;
+    }
+
+    public void setPressure2(double pressure2) {
+        this.pressure2 = pressure2;
+    }
+
+    public void setPressure3(double pressure3) {
+        this.pressure3 = pressure3;
+    }
+
+    public void setPressure4(double pressure4) {
+        this.pressure4 = pressure4;
+    }
+
+    public void setPressure5(double pressure5) {
+        this.pressure5 = pressure5;
+    }
+
+    public void setPressure6(double pressure6) {
+        this.pressure6 = pressure6;
+    }
+
+    public void setPressureCenter(double pressureCenter) {
+        this.pressureCenter = pressureCenter;
+    }
+
+    public void setProximity(double proximity) {
+        this.proximity = proximity;
+    }
+
+    public void setShearX(double shearX) {
+        this.shearX = shearX;
+    }
+
+    public void setShearY(double shearY) {
+        this.shearY = shearY;
+    }
+
+    public void updateAreaCharts() {
+        
     }
 }
