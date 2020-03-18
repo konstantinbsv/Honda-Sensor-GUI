e0763772c4aabeaa69eaf21459554c210a71afb8
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-18 12:28:57 -0700

    Change resource constants

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -6,19 +6,20 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
-    private static final String stageTitle = "Sensor GUI";
-    public static final String cssRes = "res/css/flat_dark_theme.css";
-    public static final String iconRes = "res/img/icon.png";
+    private static final String STAGE_TITLE = "Sensor GUI";
+    public static final String CSS = "css/flat_dark_theme.css";
+    public static final String ICON = "img/icon.png";
+    public static final String FXML = "ChartView.fxml";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ChartView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource(FXML));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(cssRes);
-        primaryStage.setTitle(stageTitle);
+        scene.getStylesheets().add(CSS);
+        primaryStage.setTitle(STAGE_TITLE);
         primaryStage.getIcons().add(
                 new Image(
-                        getClass().getResourceAsStream(iconRes),
+                        getClass().getResourceAsStream(ICON),
                         100, 100, true, true)); // add icon
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
